c784cef4eb0d6b5b6cacfc2230aefcd11691c2c9
Anjalinegi <anjalinegi26500@gmail.com>
2025-01-29 10:17:28 +0530

    Update LargestNo.java

--- LargestNo.java
+++ LargestNo.java
@@ -1,13 +1,18 @@
 import java.util.Scanner;
 
 public class LargestNo {
+
     public static void main(String[] args) {
+        
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt(), b = sc.nextInt(), c = sc.nextInt(); // Input three numbers
-        if (a > b && a > c) System.out.println("a is largest"); // a is largest
-        else if (b > a && b > c) System.out.println("b is largest"); // b is largest
-        else System.out.println("c is largest"); // c is largest
-        sc.close();
+        
+        if (a > b && a > c) {
+            System.out.println("a is largest"); // a is largest
+        } else if (b > a && b > c) {
+            System.out.println("b is largest"); // b is largest
+        } else {
+            System.out.println("c is largest"); // c is largest
+        }
     }
-    
 }
